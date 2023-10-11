d8451912090e7a6687c7f9c1e364501fec318b83
Wtz_LASR <wtz_lasr@outlook.com>
2023-10-12 01:27:24 +0800

    asd

--- aten/src/ATen/CheckpointTensorImpl.h
+++ aten/src/ATen/CheckpointTensorImpl.h
@@ -444,8 +444,8 @@ struct CheckpointTensorImpl : TensorImpl {
 
 struct NotifyHeapIndexChanged;
 
-bool USE_KINETIC_HEAP = true;
-int AFF_REENTRY_THRESHOLD = 2;
+inline bool USE_KINETIC_HEAP = true;
+inline int AFF_REENTRY_THRESHOLD = 2;
 
 int64_t since_epoch(time_t tp);
 
