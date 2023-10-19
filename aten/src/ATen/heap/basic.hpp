9af3543138a476ce86a10290982708902c586b58
Wtz_LASR <wtz_lasr@outlook.com>
2023-10-19 08:43:55 +0800

    Revert "k"
    This reverts commit f7bdb284af9e0d3af58b370686c4d74f31fedb44.

--- aten/src/ATen/heap/basic.hpp
+++ aten/src/ATen/heap/basic.hpp
@@ -206,8 +206,7 @@ template<typename T,
 	 typename NHIC = NotifyHeapIndexChanged<T>,
 	 typename NHER = NotifyHeapElementRemoved<T>>
 struct MinHanger : MinHeapCRTP<T, MinHanger<T, Compare, NHIC, NHER>> {
-  std::random_device seed;
-  std::mt19937 rd(seed());
+  std::mt19937 rd(std::random_device()());
 
   bool coin() {
     std::uniform_int_distribution<> distrib(0, 1);
